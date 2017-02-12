37ffa2cc3a96ddbe487fbd201813c2441d0c4745
Brandon Martinez <me@brandonmartinez.com>
2017-02-11 23:40:06 -0500

    Initial camera setup and import from Sensr

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # homebridge-camera-sensr
 A homebridge plugin to access your IP cameras available in your Sensr.net account.
+
+## Getting an OAUTH2 Token
+
+Follow the steps in the (Sensr.net guide)[http://yacc.github.io/sensrapi-tutorials/] to get a token.
+
+* _Application (client) name:_ put something along the lines of "My Family Homebridge".
+* _Main URL:_ https://github.com/nfarina/homebridge
+* _Callback URL:_ https://github.com/nfarina/homebridge
---
+++ index.js
@@ -0,0 +1,89 @@
+var Accessory, hap, UUIDGen,
+//    SensrCamera = require('./SensrCamera').SensrCamera,
+    Client = require('node-rest-client').Client,
+    client = new Client(),
+    SENSR_API_URL = 'https://api.sensr.net/u/v3/',
+    SENSR_API_CAMERAS_OWNED = SENSR_API_URL + 'cameras/owned.json';
+
+module.exports = function (homebridge) {
+    Accessory = homebridge.platformAccessory;
+    hap = homebridge.hap;
+    UUIDGen = homebridge.hap.uuid;
+
+    homebridge.registerPlatform("homebridge-camera-sensr", "Camera-Sensr", sensrPlatform, true);
+}
+
+function sensrPlatform(log, config, api) {
+    var self = this;
+
+    self.log = log;
+    self.config = config || {};
+
+    if (api) {
+        self.api = api;
+
+        if (api.version < 2.1) {
+            throw new Error("Unexpected API version.");
+        }
+
+        self.api.on('didFinishLaunching', self.didFinishLaunching.bind(this));
+    }
+}
+
+sensrPlatform.prototype.configureAccessory = function (accessory) {
+    // Won't be invoked
+}
+
+sensrPlatform.prototype.didFinishLaunching = function () {
+    var self = this;
+
+    if (self.config.accounts) {
+        // Checking for any available Sensr accounts
+        var accounts = self.config.accounts;
+        accounts.forEach(function (account) {
+            var accountDescription = account.description,
+                token = account.token;
+
+            if (!token) {
+                self.log("Missing parameters.");
+                return;
+            }
+
+            // getting a list of available cameras to register
+            var args = {
+                headers: { "Authorization": "OAUTH " + token } // request headers
+            };
+
+
+            client.get(SENSR_API_CAMERAS_OWNED, args,
+                function (data, response) {
+                    if (data && data.cameras) {
+                        var configuredAccessories = [];
+
+                        cameras.forEach(function (cameraConfig) {
+                            var camera = cameraConfig.camera,
+
+                                urls = cameraConfig.urls,
+                                sensrCameraConfig = {
+                                    name: camera.name,
+                                    id: camera.id,
+                                    state: camera.state,
+                                    still: urls.latestimage,
+                                    live: urls.livestream,
+                                },
+
+                                uuid = UUIDGen.generate(sensrCameraConfig.id),
+                                cameraAccessory = new Accessory(name, uuid, hap.Accessory.Categories.CAMERA),
+                                //cameraSource = new SensrCamera(hap, sensrCameraConfig);
+                                cameraSource = {};
+
+                            cameraAccessory.configureCameraSource(cameraSource);
+                            configuredAccessories.push(cameraAccessory);
+                        });
+
+                        self.api.publishCameraAccessories("Camera-Sensr", configuredAccessories);
+                    }
+                });
+        });
+    }
+}
